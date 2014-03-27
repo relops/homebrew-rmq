4525d598473d662120d914f643b5aea411e9a7b1
Ben Hood <0x6e6562@gmail.com>
2014-03-27 14:51:06 +0000

    Fixed hashes

--- rmq.rb
+++ rmq.rb
@@ -6,11 +6,11 @@ class Rmq < Formula
 
   if Hardware.is_64_bit?
     
-    url 'https://dl.bintray.com/relops/rmq/rmq_0.2.31_darwin_amd64.zip'
-    sha256 '61ecc7ed1da92146c1f397debc58f2604295d226b790611d9ff4750650d420cf'
+    url 'https://dl.bintray.com/relops/rmq/rmq_#{version}_darwin_amd64.zip'
+    sha256 '273e21743244bbf73f8499f326fd3c95b7a21c8f392edf1c5c5ecb79445c0f0c'
   else
-    url 'https://dl.bintray.com/relops/rmq/rmq_0.2.31_darwin_amd64.zip'
-    sha256 '2077c377e7a03cb791b447a66a75d2771ef8caa454c144950087329f0a01080c'
+    url 'https://dl.bintray.com/relops/rmq/rmq_#{version}_darwin_386.zip'
+    sha256 'e0191fee2cc8eac2b675855634f9274de6660ad8314705c1b9a9387ab376c15f'
   end
 
   depends_on :arch => :intel

3e22a86fdcf094316193013c34c6759f5ee8b43e
Ben Hood <0x6e6562@gmail.com>
2014-03-27 14:57:14 +0000

    Uprev

--- rmq.rb
+++ rmq.rb
@@ -2,15 +2,15 @@ require 'formula'
 
 class Rmq < Formula
   homepage 'https://github.com/relops/rmq'
-  version '0.2.38'
+  version '0.2.40'
 
   if Hardware.is_64_bit?
     
-    url 'https://dl.bintray.com/relops/rmq/rmq_#{version}_darwin_amd64.zip'
-    sha256 '273e21743244bbf73f8499f326fd3c95b7a21c8f392edf1c5c5ecb79445c0f0c'
+    url "https://dl.bintray.com/relops/rmq/rmq_#{version}_darwin_amd64.zip"
+    sha256 'abb2438add14e7003ae59046cdc7cf17eaedfd5d696b6661df224ca597595dbb'
   else
-    url 'https://dl.bintray.com/relops/rmq/rmq_#{version}_darwin_386.zip'
-    sha256 'e0191fee2cc8eac2b675855634f9274de6660ad8314705c1b9a9387ab376c15f'
+    url "https://dl.bintray.com/relops/rmq/rmq_#{version}_darwin_386.zip"
+    sha256 '78b43b204ddb88a904cf8b594628bd0ad488b39258d584aaf9d8cc0178ea4b15'
   end
 
   depends_on :arch => :intel
